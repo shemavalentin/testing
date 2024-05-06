626df0ce1d2f771df4f3abd5760c2d8aecc6bf8a
SHEMA Valentin <52248571+shemavalentin@users.noreply.github.com>
2024-05-06 13:54:55 +0200

    Add files via upload
    Unit testing using jestjs

---
+++ script.test.js
@@ -0,0 +1,6 @@
+//Test with jest starts with it() and most likely all tools have this method
+// It takes two parameters:  a string, and a function that needs to run.
+
+it('This is a test', () => {
+	
+})
