19fd076981a3d9cc17f69ce5fa90cc2aeb0894bf
Shema Valentin <shemavalentin@gmail.com>
2024-05-08 07:56:11 +0200

    Used jest library and machers for testing

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ const googleDatabase = [
 'myfavoritecats.com'
 	]
 
-const  googleSearch = (searchInput) => {
-	const matches = googleDatabase.filter(website => {
+// Now to make it reusable let's use something called Dependancy Injection
+// which is easy to do. We pass the function the 2nd parameter
+
+const  googleSearch = (searchInput, db) => {
+	const matches = db.filter(website => {
 		return website.includes(searchInput);
 	});
 
-	return matches.length > 3? matches.slice(0,3) : matches;
+	return matches.length >3 ?matches.slice(0,3) : matches;
 }
 
-console.log(googleSearch('cats'));
+//console.log(googleSearch('cats', googleDatabase));
+
+// Cze this is nodejs we don't have export yet rather we use commonjs to export
+// It was possible when we use Javascript or Babel
+module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,6 +1,43 @@
-//Test with jest starts with it() and most likely all tools have this method
+// Import in Node
+
+const googleSearch = require('./script');
+
+// What if we need to test the db? we can't export db and inport in here. it's very slow, and expeinsive.
+// We need to mock it up
+
+dbMock = [
+	'dog.com',
+	'cheesepuff.com',
+	'disney.com',
+	'dogpictures.com'
+	]
+
+// Here I can group all those tests
+
+describe('googleSearch', () => {
+	//Test with jest starts with it() and most likely all tools have this method
 // It takes two parameters:  a string, and a function that needs to run.
 
-it('This is a test', () => {
+it('is a silly test', () => {
+	expect('hello').toBe('hello');
 	
+});
+
+it(' is searching google', () => {
+	expect(googleSearch('testtest', dbMock)).toEqual([])
+
+	expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+});
+
+it('work with undefined and null input', ()=> {
+	expect(googleSearch(undefined, dbMock)).toEqual([]);
+
+	expect(googleSearch(null, dbMock)).toEqual([]);
+});
+
+it('does not return more than 3 matches', () => {
+	expect(googleSearch('.com', dbMock).length).toEqual(3);
 })
+
+})
+
